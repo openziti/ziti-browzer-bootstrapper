3da2b42c43ccd1f2871c7351ccbd4ae81c267d5d
Curt Tudor <curt@rentallect.com>
2022-06-21 16:12:58 -0400

    Only ZBR touches dark web app

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ var target_scheme = process.env.ZITI_AGENT_TARGET_SCHEME;
 if (typeof target_scheme === 'undefined') { target_scheme = 'https'; }
 var target_host = process.env.ZITI_AGENT_TARGET_HOST;
 var target_port = process.env.ZITI_AGENT_TARGET_PORT;
+var target_service = process.env.ZITI_AGENT_TARGET_SERVICE;
 
 /**
  * 
@@ -591,7 +592,8 @@ const startAgent = ( logger ) => {
             ziti: ziti,
             logger: logger,
             changeOrigin: true,
-            target: target_scheme + '://' + target_host + ':' + target_port,
+            // target: target_scheme + '://' + target_host + ':' + target_port,
+            target: target_scheme + '://' + target_service,
             targetPath: target_path,
 
             // Set up to rewrite 'Location' headers on redirects
