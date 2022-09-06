becb4a0d87bbf50e49874c91f63789c9506f879d
Curt Tudor <curt@rentallect.com>
2022-09-06 10:02:46 -0600

    client-side logLevel control

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ const startAgent = ( logger ) => {
         // Inject the Ziti browZer Runtime at the front of <head> element so we are prepared to intercept as soon as possible over on the browser
         let ziti_inject_html = `
 <!-- load Ziti browZer Runtime -->
-<script id="from-ziti-http-agent" type="text/javascript" src="https://${zbr_src}"></script>
+<script id="from-ziti-http-agent" type="text/javascript" src="https://${agent_host}/${common.getZBRname()}"></script>
 `;
         node.ws.write( ziti_inject_html );
 
@@ -364,7 +364,7 @@ const startAgent = ( logger ) => {
 
             session: {
                 name: 'browZerSession',
-                absoluteDuration: process.env.IDP_TOKEN_DURATION,
+                absoluteDuration: process.env.IDP_TOKEN_DURATION ? process.env.IDP_TOKEN_DURATION : 28800,
                 rolling: false,
                 cookie: {
                     httpOnly: false,    // ZBR needs to access this
