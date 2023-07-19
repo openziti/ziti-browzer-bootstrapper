23891ca56af5d7ea51fdcc3e3b0012129191acf0
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-07-19 12:13:22 -0400

    add signal handling to index.js

--- index.js
+++ index.js
@@ -508,9 +508,10 @@ const startAgent = ( logger ) => {
     /**
      * 
      */
+    var server
     if (agent_scheme === 'https') {
 
-        https.createServer({
+        server = https.createServer({
             cert: fs.readFileSync(certificate_path),
             key: fs.readFileSync(key_path),
         }, app).listen(agent_listen_port, "0.0.0.0", function() {
@@ -520,14 +521,36 @@ const startAgent = ( logger ) => {
     }
     else {
 
-        http.createServer({
+        server = http.createServer({
         }, app).listen(agent_listen_port, "0.0.0.0", function() {
             logger.info({message: 'listening', port: agent_listen_port, scheme: agent_scheme});
         });
 
     }
     /** -------------------------------------------------------------------------------------------------- */
-    
+
+    // The signals we want to handle
+    // NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+    var signals = {
+            'SIGHUP': 1,
+            'SIGINT': 2,
+            'SIGTERM': 15
+        };
+    // Do any necessary shutdown logic for our application here
+    const shutdown = (signal, value) => {
+        console.log("shutdown!");
+        server.close(() => {
+            console.log(`server stopped by ${signal} with value ${value}`);
+            process.exit(128 + value);
+        });
+    };
+    // Create a listener for each of the signals that we want to handle
+    Object.keys(signals).forEach((signal) => {
+        process.on(signal, () => {
+            console.log(`process received a ${signal} signal`);
+            shutdown(signal, signals[signal]);
+        });
+    });
 };
 
 
