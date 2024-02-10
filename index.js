fa1798739fd2b25b0eb7355ead9bd75dc0b22273
Curt Tudor <curt@rentallect.com>
2024-02-10 10:05:16 -0700

    OIDC refactor

--- index.js
+++ index.js
@@ -83,21 +83,10 @@ var logger;     // for Ziti BrowZer Bootstrapper
                             "https"
                         ]
                     },
-                    "idp_type": {
-                        "type": "string",
-                        "enum": [
-                            "auth0", 
-                            "azure_ad",
-                            "keycloak",
-                        ]
-                    },
                     "idp_issuer_base_url": {
                         "type": "string"
                     },
                     "idp_client_id": {
-                        "type": "string"
-                    },
-                    "idp_realm": {
                         "type": "string"
                     },
                 },
@@ -287,12 +276,6 @@ const startBootstrapper =  async ( logger ) => {
             zbrSrc = `${req.ziti_browzer_bootstrapper_scheme}://${req.ziti_vhost}:${browzer_bootstrapper_listen_port}/${common.getZBRname()}`;
         }
         let thirdPartyHTML = '';
-        if (req.ziti_idp_type === 'keycloak') {
-            thirdPartyHTML = `
-<!-- load Keycloak Adapter -->
-<script src="https://cdn.jsdelivr.net/npm/keycloak-js@23.0.1/dist/keycloak.min.js"></script>        
-`;            
-        }
         if (req.ziti_load_eruda) {
             console.log(`loading ERUDA`);
 
@@ -509,8 +492,6 @@ ${thirdPartyHTML}
         
             req.ziti_idp_issuer_base_url = target.idp_issuer_base_url;
             req.ziti_idp_client_id   = target.idp_client_id;
-            req.ziti_idp_type        = target.idp_type;
-            req.ziti_idp_realm       = target.idp_realm;
 
             req.ziti_load_eruda      = req.query.eruda ? true : false;
 
@@ -653,7 +634,7 @@ ${thirdPartyHTML}
             }
         );
 
-        cron.schedule('* * * * *', () => {      // run every midnight
+        cron.schedule('1,31 * * * *', () => {      // run every half-hour
             const { validTo } = new X509Certificate(fs.readFileSync( certificate_path));
             var validToDate = new Date(validTo);
             var validToTime = validToDate.getTime() / 1000;
